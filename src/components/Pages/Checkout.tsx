b67471a77dac6d88868c31f2fd47c220bb5aad89
kid-us <kidushh29@gmail.com>
2024-09-03 14:08:29 +0300

    Promo modal on the checkout

--- src/components/Pages/Checkout.tsx
+++ src/components/Pages/Checkout.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import z from "zod";
@@ -20,54 +20,12 @@ const schema = z.object({
   address: z.string().min(3, {
     message: "Address must be at least than 5 chars.",
   }),
-  promo: z.string().optional(),
 });
 
 type FormData = z.infer<typeof schema>;
 
 const Checkout = () => {
   const { id } = useParams();
-  console.log(id);
-
-  const [maxDate, setMaxDate] = useState<string>("");
-
-  // Date Input
-  useEffect(() => {
-    // Get today's date
-    const today = new Date();
-    // Extract the year, month, and day
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, "0");
-    const day = String(today.getDate()).padStart(2, "0");
-    // Format the date as YYYY-MM-DD
-    const formattedDate = `${year}-${month}-${day}`;
-    // Set the max date
-    setMaxDate(formattedDate);
-  }, []);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({ resolver: zodResolver(schema) });
-
-  const [loader, setLoader] = useState<boolean>(false);
-  const [success, setSuccess] = useState<boolean>(false);
-  const [paidDate, setPaidDate] = useState<Date | null>(null);
-  const [dateError, setDateError] = useState(false);
-
-  // On form Submit
-  const onSubmit = (data: FieldValues) => {
-    if (paidDate === null) {
-      setDateError(true);
-      return;
-    }
-
-    setLoader(true);
-    setSuccess(true);
-
-    console.log(data);
-  };
 
   const [defaultShoe] = useState<Shoes>({
     id: 1,
@@ -77,8 +35,81 @@ const Checkout = () => {
     price: 2999,
   });
 
+  const [promo, setPromo] = useState<boolean>(true);
+  const [promoCode, setPromoCode] = useState<string>("");
+  const [promoError, setPromoError] = useState<boolean>(false);
+
+  // Handle Promo Submit
+  const handlePromoSubmit = () => {
+    if (promoCode.length < 5) {
+      setPromoError(true);
+      return;
+    }
+    setPromoError(false);
+    setPromo(false);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const [loader, setLoader] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  // On form Submit
+  const onSubmit = (data: FieldValues) => {
+    setLoader(true);
+    setSuccess(true);
+
+    console.log(data, id);
+  };
+
   return (
     <>
+      {/* Promo */}
+      {promo && (
+        <>
+          <div className="overlay top-0 z-50"></div>
+          <div className="fixed top-0 flex z-50 justify-center items-center h-[100dvh] w-full lg:px-0 px-3">
+            <div className="bg-white w-[500px] p-5 rounded-lg pb-7 ">
+              <div className="flex justify-between mb-4">
+                <p className="font-bold text-lg">Notice</p>
+                <button
+                  onClick={() => setPromo(false)}
+                  className="bi-x-lg text-lg"
+                ></button>
+              </div>
+              <p className="mt-2 mb-4 text-sm font-bold">
+                Have a promo code? Enter it in the field below to enjoy an
+                exclusive{" "}
+                <span className="text-green-500 uppercase">discount</span> on
+                your purchase!
+              </p>
+
+              <input
+                name="promo_code"
+                type="text"
+                className="rounded focus:outline-none shadow shadow-zinc-900 h-12 text-gray-800 px-5 bg-white block w-full placeholder:text-gray-500"
+                placeholder="Promo code"
+                value={promoCode}
+                onChange={(e) => setPromoCode(e.currentTarget.value)}
+              />
+              {promoError && (
+                <p className="text-xs text-red-600 pt-1">Insert promo code</p>
+              )}
+              <button
+                onClick={handlePromoSubmit}
+                className="btn-bg w-full rounded mt-3 h-11 shadow shadow-zinc-900"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+
       {/* Payment Done */}
       {success && (
         <>
@@ -132,113 +163,79 @@ const Checkout = () => {
               payer, and the date you made the payment. That's it.
             </p>
 
-            <hr className="my-4 border-gray-300" />
+            <hr className="mt-4 border-gray-300" />
 
-            <div className="pt-3">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                {/* Name */}
-                <div className="my-4">
-                  <label
-                    htmlFor="name"
-                    className="block text-sm mb-2 text-gray-600"
-                  >
-                    Transfer By
-                  </label>
-                  <input
-                    {...register("name")}
-                    name="name"
-                    type="text"
-                    className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
-                  />
-                  {errors.name && (
-                    <p className="text-xs text-red-600 mb-5 mt-2">
-                      {errors.name.message}
-                    </p>
-                  )}
-                </div>
-                {/* ID */}
-                <div className="my-4">
-                  <label
-                    htmlFor="id"
-                    className="block text-sm mb-2 text-gray-600"
-                  >
-                    Transaction Number / ID
-                  </label>
-                  <input
-                    {...register("transId")}
-                    type="text"
-                    className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
-                  />
-                  {errors.transId && (
-                    <p className="text-xs text-red-600 mb-5 mt-2">
-                      {errors.transId.message}
-                    </p>
-                  )}
-                </div>
-                {/* Address */}
-                <div className="my-4">
-                  <label
-                    htmlFor="id"
-                    className="block text-sm mb-2 text-gray-600"
-                  >
-                    Address
-                  </label>
-                  <input
-                    {...register("address")}
-                    type="text"
-                    className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
-                  />
-                  {errors.address && (
-                    <p className="text-xs text-red-600 mb-5 mt-2">
-                      {errors.address.message}
-                    </p>
-                  )}
-                </div>
-                {/* Date */}
-                <div className="my-4">
-                  <label
-                    htmlFor="id"
-                    className="block text-sm mb-2 text-gray-600"
-                  >
-                    Transaction Date
-                  </label>
-                  <input
-                    type="date"
-                    className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
-                    onChange={(e) => setPaidDate(new Date(e.target.value))}
-                    max={maxDate}
-                  />
-                  {dateError && (
-                    <p className="text-xs text-red-600 mb-5 mt-2">
-                      Date required
-                    </p>
-                  )}
-                </div>
-
-                {/* Promo */}
-                <div className="my-4">
-                  <label
-                    htmlFor="promo"
-                    className="block text-sm mb-2 text-gray-600"
-                  >
-                    Do u have promo code? insert here.
-                  </label>
-                  <input
-                    {...register("promo")}
-                    type="promo"
-                    className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
-                  />
-                </div>
-
-                {/* Button */}
-                <div className="mt-8">
-                  {loader ? <Loader /> : <Button label="Submit Payment" />}
-                  <p className="text-xs text-gray-500 mt-4">
-                    Your personal data will be used to process your order.
+            {/* Form */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* Name */}
+              <div className="my-4">
+                <label
+                  htmlFor="name"
+                  className="block text-sm mb-2 text-gray-600"
+                >
+                  Transfer By
+                </label>
+                <input
+                  {...register("name")}
+                  name="name"
+                  type="text"
+                  className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
+                />
+                {errors.name && (
+                  <p className="text-xs text-red-600 mb-5 mt-2">
+                    {errors.name.message}
                   </p>
-                </div>
-              </form>
-            </div>
+                )}
+              </div>
+
+              {/* ID */}
+              <div className="my-4">
+                <label
+                  htmlFor="id"
+                  className="block text-sm mb-2 text-gray-600"
+                >
+                  Transaction Number / ID
+                </label>
+                <input
+                  {...register("transId")}
+                  type="text"
+                  className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
+                />
+                {errors.transId && (
+                  <p className="text-xs text-red-600 mb-5 mt-2">
+                    {errors.transId.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Address */}
+              <div className="my-4">
+                <label
+                  htmlFor="id"
+                  className="block text-sm mb-2 text-gray-600"
+                >
+                  Address
+                </label>
+                <input
+                  {...register("address")}
+                  type="text"
+                  className="rounded focus:outline-none shadow shadow-zinc-900 h-14 w-full text-gray-800 px-5 bg-white"
+                />
+                {errors.address && (
+                  <p className="text-xs text-red-600 mb-5 mt-2">
+                    {errors.address.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Button */}
+              <div className="mt-8">
+                {loader ? <Loader /> : <Button label="Submit Payment" />}
+                <p className="text-xs text-gray-500 mt-4">
+                  Your personal data will be used to process your order.
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>
