bf92890f5bac4ad5a6161ca788f5656045bafb3e
kid-us <kidushh29@gmail.com>
2024-09-04 16:00:36 +0300

    Loading animation changed

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,9 +1,11 @@
+import { logo_sm } from "../../assets";
 import "./loading.css";
 const Loading = () => {
   return (
     <div className="fixed z-50 bg-primary h-full w-full ">
       <div className="flex justify-center items-center h-full">
-        <p className="loading"></p>
+        {/* <p className="loading"></p> */}
+        <img src={logo_sm} alt="logo" className="w-24 animate-pulse" />
       </div>
     </div>
   );
