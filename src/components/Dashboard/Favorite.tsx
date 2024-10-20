ac58edbeb39e3befa3c7f6fd22d9ca3add08d644
Kid-us <kidushh29@gmail.com>
2024-10-20 14:07:04 +0300

    Displaying Favorite pages

--- src/components/Dashboard/Favorite.tsx
+++ src/components/Dashboard/Favorite.tsx
@@ -1,11 +1,69 @@
 import { Link } from "react-router-dom";
-import shoes from "../../services/shoes";
 import { useCartStore } from "../../stores/useCartStore";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import useFavorite from "../../hooks/useFavorite";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import baseUrl from "../../services/request";
 
 const Favorite = () => {
   const { addToCart, cart } = useCartStore();
+
+  const { favorite } = useFavorite();
+  const access_token = localStorage.getItem("token");
+  const [favoriteShoe, setFavoriteShoe] = useState<number[]>([]);
+
+  // Set Favorites
+  useEffect(() => {
+    if (favorite.length > 0) {
+      const favoriteIds = favorite.map((f) => f.id); // Extract IDs from the favorite array
+      setFavoriteShoe(favoriteIds); // Directly set the favoriteShoe state with the array of IDs
+    }
+  }, [favorite]);
+
+  // Adding Removing Favorite
+  const handleFavorite = (id: number) => {
+    if (favoriteShoe.includes(id)) {
+      // Remove the id from the state
+      axios
+        .post(
+          `${baseUrl}auth/remove-from-favorite?id=${id}`,
+          {},
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${access_token}`,
+            },
+          }
+        )
+        .then(() => {
+          setFavoriteShoe(favoriteShoe.filter((favId) => favId !== id));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      // Add the id to the state
+      axios
+        .post(
+          `${baseUrl}auth/add-to-favorite?id=${id}`,
+          {},
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${access_token}`,
+            },
+          }
+        )
+        .then(() => {
+          setFavoriteShoe([...favoriteShoe, id]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
 
   return (
     <>
@@ -30,69 +88,82 @@ const Favorite = () => {
           </Link>
         </div>
         <p className="mb-4">You favorite shoes are here</p>
-        {/* Favorite */}
-        <div className="lg:grid grid-cols-2 gap-5">
-          {shoes.map((shoe) => (
-            <div
-              key={shoe.id}
-              className="relative bg-gray-50 rounded-2xl shadow shadow-zinc-500 p-5 lg:mb-0 mb-10"
-            >
-              <button className="absolute top-7 z-10 text-xl overflow-hidden cursor-default text-red-500 bi-heart right-2 w-20 h-10"></button>
-              <div className="flex justify-center bg rounded-2xl hover:rotate-0 shadow-inner p-2">
-                <Link
-                  to={`/shoes/${shoe.id}`}
-                  className="w-full overflow-hidden"
-                >
-                  <img
-                    src={shoe.img}
-                    alt="Shoe"
-                    className={`relative z-10 h-72 w-full object-contain -rotate-[20deg] hover:rotate-0`}
-                  />
-                </Link>
-              </div>
+        {favorite.length > 0 ? (
+          <div className="lg:grid grid-cols-3 gap-5">
+            {favorite.map((shoe) => (
+              <div
+                key={shoe.id}
+                className="relative bg-gray-50 rounded-2xl shadow shadow-zinc-500 p-5 lg:mb-0 mb-10"
+              >
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleFavorite(shoe.id);
+                  }}
+                  className={`${
+                    favoriteShoe.includes(shoe.id)
+                      ? "bi-heart-fill"
+                      : "bi-heart"
+                  } absolute top-7 z-50 bg-transparent text-xl overflow-hidden cursor-default text-red-500 right-2 w-20 h-20`}
+                ></button>
 
-              <div className="mt-5">
-                <div className="flex justify-between">
-                  <p className="font-extrabold text-xl">{shoe.name}</p>
-                  <button
-                    onClick={() =>
-                      addToCart({
-                        id: shoe.id,
-                        quantity: 1,
-                        size: 0,
-                        img: shoe.img,
-                        price: shoe.price,
-                      })
-                    }
-                    className={`font-extrabold text-2xl ${
-                      cart.some((c) => c.id === shoe.id)
-                        ? "bi-bag-fill text-white bg-cyan-600 rounded-full w-8 h-8 text-lg"
-                        : "bi-bag"
-                    }`}
-                  ></button>
+                <div className="flex justify-center bg rounded-2xl hover:rotate-0 shadow-inner p-2">
+                  <Link
+                    to={`/shoes/${shoe.id}`}
+                    className="w-full overflow-hidden"
+                  >
+                    <img
+                      src={shoe.main_picture}
+                      alt="Shoe"
+                      className={`relative z-10 h-72 w-full object-contain -rotate-[20deg] hover:rotate-0`}
+                    />
+                  </Link>
                 </div>
-                <p className="mt-1 font-bold">
-                  <span className="bi-cash text-xl me-1"></span> {shoe.price}br
-                </p>
+
+                <div className="mt-5">
+                  <div className="flex justify-between">
+                    <p className="font-extrabold text-xl">{shoe.name}</p>
+                    <button
+                      onClick={() =>
+                        addToCart({
+                          id: shoe.id,
+                          quantity: 1,
+                          size: 0,
+                          img: shoe.main_picture,
+                          price: shoe.price,
+                        })
+                      }
+                      className={`font-extrabold text-2xl ${
+                        cart.some((c) => c.id === shoe.id)
+                          ? "bi-bag-fill text-white bg-cyan-600 rounded-full w-8 h-8 text-lg"
+                          : "bi-bag"
+                      }`}
+                    ></button>
+                  </div>
+                  <p className="mt-1 font-bold">
+                    <span className="bi-cash text-xl me-1"></span> {shoe.price}
+                    br
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        {/* Empty */}
-        <div className="flex justify-center items-center h-[40dvh]">
-          <div className="bg-white rounded px-3 py-5 shadow shadow-zinc-900">
-            <p className="mb-4">
-              You haven't add your favorite shoes yet, but when you do, they
-              will be displayed here.
-            </p>
-            <Link
-              to={"/"}
-              className="btn-bg rounded py-2 px-10 text-white shadow shadow-zinc-900 font-bold"
-            >
-              Shop Now
-            </Link>
+            ))}
           </div>
-        </div>
+        ) : (
+          <div className="flex justify-center items-center h-[40dvh]">
+            <div className="bg-white rounded px-3 py-5 shadow shadow-zinc-900">
+              <p className="mb-4">
+                You haven't add your favorite shoes yet, but when you do, they
+                will be displayed here.
+              </p>
+              <Link
+                to={"/"}
+                className="btn-bg rounded py-2 px-10 text-white shadow shadow-zinc-900 font-bold"
+              >
+                Shop Now
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
       <Footer />
     </>
