355fce5a0d71acdc9f289aed000edf99bb6b2e31
Kid-us <kidushh29@gmail.com>
2024-10-29 08:40:47 +0300

    Verify email added success and expire

--- src/components/Pages/Verify.tsx
+++ src/components/Pages/Verify.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { logo_lg } from "../../assets";
 
 const Verify: React.FC = () => {
@@ -11,9 +11,13 @@ const Verify: React.FC = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const email = searchParams.get("email");
+  const expire = searchParams.get("expire");
+  const success = searchParams.get("success");
 
   useEffect(() => {
-    !email && navigate("/login");
+    if (!email && !expire && !success) {
+      navigate("/login");
+    }
   }, []);
 
   return (
@@ -23,11 +27,46 @@ const Verify: React.FC = () => {
           <div className="flex justify-center mb-10">
             <img src={logo_lg} alt="Logo" className="w-28" />
           </div>
-          <p className="text-lg bg-white p-4 rounded">
-            Check your email. We’ve sent a confirmation link to{" "}
-            <span className="text-blue-500">{email}</span>, Please complete your
-            action.
-          </p>
+          {/* Verify */}
+          {email && (
+            <p className="text-lg p-4 rounded">
+              Check your email. We’ve sent a confirmation link to{" "}
+              <span className="text-blue-500">{email}</span>, Please complete
+              your action.
+            </p>
+          )}
+          {/* Success */}
+          {success && (
+            <div className="">
+              <p className="bi-check-circle-fill text-center text-green-500 text-4xl mb-4"></p>
+              <p className="mb-4">
+                Congratulations! Your email has been successfully verified. You
+                can now access all the features of your account.
+              </p>
+              <Link to="/login">
+                <p className="py-3 text-center text-black btn-bg w-full rounded font-poppins shadow-sm shadow-black text-s font-bold">
+                  Login
+                </p>
+              </Link>
+            </div>
+          )}
+          {/* Expire */}
+          {expire && (
+            <>
+              <p className="bi-exclamation-triangle-fill text-center text-red-500 text-4xl mb-4"></p>
+
+              <p className="mb-4">
+                It looks like you haven't verified your email yet. For security
+                purposes, your verification link is expired. Please click the
+                button below to complete your email verification.
+              </p>
+              <Link to="/register">
+                <p className="py-3 text-center text-black btn-bg w-full rounded font-poppins shadow-sm shadow-black text-s font-bold">
+                  Register
+                </p>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
